d038fb45291375a2f38b7cea777228b8b0d466da
PAPerfilTeste <110242063+PArthur006@users.noreply.github.com>
2025-02-04 12:05:31 -0300

    Falling Platforms finish
    Plataformas que caem após o player entrar em contato. Todas posicionadas e funcionais.

--- dark-tower-knight/scripts/falling_platform.gd
+++ dark-tower-knight/scripts/falling_platform.gd
@@ -1,7 +1,7 @@
 extends AnimatableBody2D
 
-@onready var anim := $Animation as AnimationPlayer
-@onready var respawn_timer := $Respawn as Timer
+@onready var anim := $anim as AnimationPlayer
+@onready var respawn_timer := $respawn as Timer
 @onready var respawn_position := global_position
 
 @export var reset_timer := 3.0
@@ -10,20 +10,28 @@ var velocity := Vector2.ZERO
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var is_triggered := false
 
-func _ready() -> void:
-	set_physics_process(false) 
+func _ready():
+	set_physics_process(false)
 
-
-func _physics_process(delta):
+func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
 	position += velocity * delta
-
 
 func has_collided_with(collision: KinematicCollision2D, collider: CharacterBody2D):
 	if !is_triggered:
 		is_triggered = true
-		anim.play("Shake")
+		anim.play("shake")
 		velocity = Vector2.ZERO
 
-func _on_animation_animation_finished(anim_name):
+
+func _on_anim_animation_finished(anim_name: StringName) -> void:
 	set_physics_process(true)
+	respawn_timer.start(reset_timer)
+	
+func _on_respawn_timeout() -> void:
+	set_physics_process(false)
+	global_position = respawn_position
+	if is_triggered:
+		var spawn_tween=  create_tween().set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_IN_OUT)
+		spawn_tween.tween_property($texture, "scale", Vector2(1, 1), 0.2).from(Vector2(0, 0))
+	is_triggered = false
