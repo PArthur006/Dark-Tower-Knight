d4222228e5659804877363c0133ba93ef3087277
MLuana725 <marialuana.sl962@gmail.com>
2025-02-09 10:32:34 -0300

    Add files via upload

--- dark-tower-knight/assets/mobs/ESQUELETO/Esqueleto.gd
+++ dark-tower-knight/assets/mobs/ESQUELETO/Esqueleto.gd
@@ -20,32 +20,40 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	
 	if wall_det.is_colliding(): #processo para virar
-		direction = -1
+		direction *= -1
 		wall_det.scale.x *= -1
 	
 	if direction == 1:
+		$Hitbox/CollisionShape2D.position.x = 9
 		anim.flip_h = false
 	else:
+		$Hitbox/CollisionShape2D.position.x = -9
 		anim.flip_h = true
 	velocity.x = direction * gravity * delta
 	move_and_slide()
 
-func take_damage(amount:int):
-	health -= amount
+func take_damage(amount: int):
+	health = health - 25 
+	anim.play("Hit")
+	await get_tree().create_timer(0.5).timeout
+	anim.play("Walk")
 	if health <= 0:
 		die()
 
 func die():
 	anim.play("Death")
+	await get_tree().create_timer(1.5).timeout
 	queue_free()
 	
 
 #Ataque esqueleto
 func _on_hitbox_body_entered(body):
-	if body.is_in_group("Player") and not body.is_attacking:
+	if body.is_in_group("Player") and not body.is_atacking:
+		anim.play("Attack")
 		body.take_damage(damage)
 
 #Levou dano
 func _on_hurt_box_body_entered(body):
-	if body.is_in_group("Player") and body.is_attacking:
-		take_damage(body.attack_damage)
+	if body.is_in_group("Player") and body.is_atacking:
+		print("Esqueleto",health)
+		take_damage(body.atack_damage)
